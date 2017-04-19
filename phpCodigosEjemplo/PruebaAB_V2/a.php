4e7ffd28b464f01482f08b4db0cc5bafab098f61
osukaru16 <osukaru16@gmail.com>
2017-04-19 10:35:32 +0200

    Add files via upload

---
+++ phpCodigosEjemplo/PruebaAB_V2/a.php
@@ -0,0 +1,21 @@
+<?php
+//namespace foo/bar;
+
+class A
+{
+   private $palabra = "Neko";
+
+   public function getPalabra(){
+        return $this->palabra; 
+   }
+
+
+}
+
+/*
+$prueba = new A();
+
+echo "Prueba ".$prueba->getPalabra();
+*/
+
+?>
---
+++ phpCodigosEjemplo/PruebaAB_V2/b.php
@@ -0,0 +1,10 @@
+<?php
+include "a.php";
+
+//$prueba = new foo/bar/A(); // posiblemente funciona pero requiere minimo php 5.3
+
+$prueba = new A();
+
+echo $prueba->getPalabra();
+
+?>
