9d1de5595c1c56ee9a5a047384fe04e88b15fc66
osukaru16 <osukaru16@gmail.com>
2017-04-24 09:03:12 +0200

    Add files via upload

---
+++ phpCodigosEjemplo/ej_to_string.php
@@ -0,0 +1,20 @@
+<?php
+// Declarar una clase simple
+class TestClass
+{
+    public $foo;
+
+    public function __construct($foo)
+    {
+        $this->foo = $foo;
+    }
+
+    public function __toString()
+    {
+        return $this->foo;
+    }
+}
+
+$class = new TestClass('Hola Mundo');
+echo $class;
+?>
